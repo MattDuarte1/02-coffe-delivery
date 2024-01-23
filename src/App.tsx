89c2f81afb8915a5f832ceb84d9aa113fe5e4b7d
Mateus <mateus_duarte20@outlook.com>
2024-01-23 16:54:15 -0300

    feat: adicionado o provider de rotas no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Header } from './components/Header'
-import { Home } from './pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { RoutesApp } from './routes'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <RoutesApp />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
